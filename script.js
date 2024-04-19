7028c2930c8f9003f12aa96575e2fb81ccb83231
Mehdi Charef <me-charef@hotmail.fr>
2024-04-19 20:35:19 +0200

    png to svg

--- script.js
+++ script.js
@@ -1,28 +1,89 @@
+// function handleCarousel() {
+//   const slides = document.querySelectorAll('.carousel__slide');
+//   const nextBtn = document.querySelector('.carousel__control--next');
+//   const prevBtn = document.querySelector('.carousel__control--prev');
+//   let currentSlide = 0;
+
+//   nextBtn.addEventListener('click', () => {
+//     currentSlide++;
+
+//     if (currentSlide >= slides.length) {
+//       currentSlide = 0;
+//     }
+
+//     updateSlide();
+//   });
+
+//   prevBtn.addEventListener('click', () => {
+//     currentSlide--;
+
+//     if (currentSlide < 0) {
+//       currentSlide = slides.length - 1;
+//     }
+
+//     updateSlide();
+//   });
+
+//   function updateSlide() {
+//     slides.forEach((slide) => {
+//       slide.style.display = 'none';
+//     });
+
+//     slides[currentSlide].style.display = 'flex';
+//   }
+// }
+// handleCarousel();
+
 function handleCarousel() {
   const slides = document.querySelectorAll('.carousel__slide');
   const nextBtn = document.querySelector('.carousel__control--next');
   const prevBtn = document.querySelector('.carousel__control--prev');
   let currentSlide = 0;
+  let touchStartX = 0;
+  let touchEndX = 0;
 
   nextBtn.addEventListener('click', () => {
-    currentSlide++;
-
-    if (currentSlide >= slides.length) {
-      currentSlide = 0;
-    }
-
-    updateSlide();
+    changeSlide(1);
   });
 
   prevBtn.addEventListener('click', () => {
-    currentSlide--;
+    changeSlide(-1);
+  });
 
-    if (currentSlide < 0) {
+  // Ajout des gestionnaires d'événements tactiles
+  document
+    .querySelector('.carousel__slides')
+    .addEventListener('touchstart', (e) => {
+      touchStartX = e.changedTouches[0].screenX;
+    });
+
+  document
+    .querySelector('.carousel__slides')
+    .addEventListener('touchend', (e) => {
+      touchEndX = e.changedTouches[0].screenX;
+      handleTouchMove();
+    });
+
+  function handleTouchMove() {
+    if (touchEndX < touchStartX - 75) {
+      changeSlide(1);
+    }
+    if (touchEndX > touchStartX + 75) {
+      changeSlide(-1);
+    }
+  }
+
+  function changeSlide(direction) {
+    currentSlide += direction;
+
+    if (currentSlide >= slides.length) {
+      currentSlide = 0;
+    } else if (currentSlide < 0) {
       currentSlide = slides.length - 1;
     }
 
     updateSlide();
-  });
+  }
 
   function updateSlide() {
     slides.forEach((slide) => {
@@ -32,6 +93,7 @@ function handleCarousel() {
     slides[currentSlide].style.display = 'flex';
   }
 }
+
 handleCarousel();
 
 //handle collapses
